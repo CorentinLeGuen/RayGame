a941208f64ef22ccb4a921ac348bb60c8e035245
CorentinleGuen <leguen.corentin42@gmail.com>
2020-05-22 12:41:08 -0400

    Reformat main and add frames, FPS and mouse position

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import os
+
 os.environ["RAYLIB_BIN_PATH"] = "raylib-2.0.0-Win64-mingw/lib/"
 
 from raylibpy import *
 
+sizeScreenWidth = 1600
+sizeScreenHeight = 900
+
+targetFPS = 60
+
 
 def main():
+    init_window(sizeScreenWidth, sizeScreenHeight, "RayGame ! - v1.0.0")
 
-    init_window(800, 450, "raylib [core] example - basic window")
+    set_target_fps(targetFPS)
 
-    set_target_fps(60)
+    frame_counter = 0
 
     while not window_should_close():
+        mouse_position = get_mouse_position()
+
+        if frame_counter >= targetFPS:
+            frame_counter = 0
+        else:
+            frame_counter += 1
 
         begin_drawing()
         clear_background(RAYWHITE)
-        draw_text("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
+
+        draw_text("Frame : " + str(frame_counter), 10, 10, 12, BLACK)
+        draw_text("FPS : " + str(get_fps()), 10, 30, 12, BLACK)
+        draw_text("Mouse X : " + str(int(mouse_position.x)), 10, 50, 12, BLACK)
+        draw_text("Mouse Y : " + str(int(mouse_position.y)), 10, 70, 12, BLACK)
+
         end_drawing()
 
     close_window()
