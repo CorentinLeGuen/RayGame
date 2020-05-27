b8dea5d657bdcd03118ffc71a3e764bb6e5b572b
CorentinleGuen <leguen.corentin42@gmail.com>
2020-05-26 20:40:43 -0400

    Draw a gris and move the origin

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A map creator in Python using [RayLib](https://www.raylib.com/).
 ## Install
 
 I am using a _venv_ install. Do a `pip install raylib-py` to install Raylib.
+
+## Feature list
+
+- Matrix
+- Save JSON map / Load
+- Config file
--- main.py
+++ main.py
@@ -5,22 +5,30 @@ from sprites import FileSprite
 os.environ["RAYLIB_BIN_PATH"] = "raylib-2.0.0-Win64-mingw/lib/"
 
 from raylibpy import *
+from grid.matrix import Matrix
 
-sizeScreenWidth = 1600
-sizeScreenHeight = 900
+SIZE_SCREEN_WIDTH = 1600
+SIZE_SCREEN_HEIGHT = 900
 
-targetFPS = 60
+SIZE_SMALL_TEXT = 11
+SIZE_MEDIUM_TEXT = 13
+SIZE_BIG_TEXT = 15
+
+SIZE_RENDER_SPRITE: int = 21
+
+TARGET_FPS = 60
 
 
 def main():
-    init_window(sizeScreenWidth, sizeScreenHeight, "RayGame ! - v1.0.0")
+    init_window(SIZE_SCREEN_WIDTH, SIZE_SCREEN_HEIGHT, "RayGame ! - v1.0.0")
 
-    set_target_fps(targetFPS)
+    set_target_fps(TARGET_FPS)
     # ======================================================================
     #   Start definition
     # ======================================================================
 
     frame_counter = 0
+    originGrisPosition: Vector2 = Vector2(0, 0)
 
     # Import the texture pack
     sprite_file = FileSprite('resources/sprites/map.png')
@@ -28,6 +36,10 @@ def main():
     # Define sprites from the texture pack
     sprite_file.add_sprite(Rectangle(16, 0, 16, 16), 'grass')
     sprite_file.add_sprite(Rectangle(32, 0, 16, 16), 'ground')
+    sprite_file.add_sprite(Rectangle(0, 16, 48, 64), 'house')
+
+    # The map
+    grid = Matrix(20, 20)
 
     # ======================================================================
     #   End definition
@@ -36,7 +48,7 @@ def main():
     while not window_should_close():
         mouse_position: Vector2 = get_mouse_position()
 
-        if frame_counter >= targetFPS:
+        if frame_counter >= TARGET_FPS:
             frame_counter = 0
         else:
             frame_counter += 1
@@ -48,10 +60,38 @@ def main():
         #   Start drawing
         # ======================================================================
 
-        draw_text("Frame : " + str(frame_counter), 10, 10, 12, BLACK)
-        draw_text("FPS : " + str(get_fps()), 10, 30, 12, BLACK)
-        draw_text("Mouse X : " + str(int(mouse_position.x)), 10, 50, 12, BLACK)
-        draw_text("Mouse Y : " + str(int(mouse_position.y)), 10, 70, 12, BLACK)
+        # Write some infos
+        draw_text("Frame : " + str(frame_counter), 10, 10, SIZE_SMALL_TEXT, BLACK)
+        draw_text("FPS : " + str(get_fps()), 10, 30, SIZE_SMALL_TEXT, BLACK)
+        draw_text("Mouse X : " + str(int(mouse_position.x)), 10, 50, SIZE_SMALL_TEXT, BLACK)
+        draw_text("Mouse Y : " + str(int(mouse_position.y)), 10, 70, SIZE_SMALL_TEXT, BLACK)
+
+        # Draw the grid
+        for x in range(grid.get_row_number() + 1):
+            for y in range(grid.get_col_number() + 1):
+                # Vertical lines
+                draw_line(0 + originGrisPosition.x,
+                          y * SIZE_RENDER_SPRITE + originGrisPosition.y,
+                          grid.get_col_number() * SIZE_RENDER_SPRITE + originGrisPosition.x,
+                          y * SIZE_RENDER_SPRITE + originGrisPosition.y,
+                          BLACK)
+                # Horizontal lines
+                draw_line(x * SIZE_RENDER_SPRITE + originGrisPosition.x,
+                          0 + originGrisPosition.y,
+                          x * SIZE_RENDER_SPRITE + originGrisPosition.x,
+                          grid.get_row_number() * SIZE_RENDER_SPRITE + originGrisPosition.y,
+                          BLACK)
+
+        # ===== Key events =====================================================
+        # === Move the origin
+        if is_key_pressed(KEY_RIGHT) or is_key_down(KEY_RIGHT):
+            originGrisPosition.x += 1
+        if is_key_pressed(KEY_LEFT) or is_key_down(KEY_LEFT):
+            originGrisPosition.x -= 1
+        if is_key_pressed(KEY_UP) or is_key_down(KEY_UP):
+            originGrisPosition.y -= 1
+        if is_key_pressed(KEY_DOWN) or is_key_down(KEY_DOWN):
+            originGrisPosition.y += 1
 
         # ======================================================================
         #   End drawing
